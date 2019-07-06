05affc231622802ba5b19d7300769f2c33211e5d
Tim Jones <tim.jones@clevertech.biz>
2019-07-05 20:44:23 -0700

    Calling mock data from API

--- client/src/app/book.service.ts
+++ client/src/app/book.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Book } from './book';
 import { BOOKS } from './mock-books';
 import { Observable, of } from 'rxjs';
@@ -8,9 +10,13 @@ import { Observable, of } from 'rxjs';
 })
 export class BookService {
 
-  constructor() { }
+  private bookUrl = 'http://localhost:3000/books';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   public getBooks(): Observable<Book[]> {
-    return of(BOOKS);
+    return this.httpClient.get<Book[]>(this.bookUrl);
   }
 }
